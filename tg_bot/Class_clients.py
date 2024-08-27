83d674ffc6487044a440d0c6bed5345619d572ed
Nikita Usachev <usachevnv@mail.ru>
2024-08-27 21:02:58 +0300

    abc

--- tg_bot/Class_clients.py
+++ tg_bot/Class_clients.py
@@ -1,3 +1,5 @@
+import os.path
+
 from Class_client import Client
 import json
 
@@ -26,10 +28,10 @@ class Clients():
         if mark_for_client:
             mark_for_client.Client_comp_training()
 
-    def Clients_paid_done(self, name, paid):
+    def Clients_paid_done(self, name, paid, cost):
         paid_client = self.Clients_finde(name)
         if paid_client:
-            paid_client.Client_paid_training(paid)
+            paid_client.Client_paid_training(paid, cost)
 
 
     def Clients_rename(self, old_name, new_name):
@@ -39,15 +41,18 @@ class Clients():
             client_for_rename.name = new_name
 
     def __load_from_json(self):
-        with open(self.__json_file_name, 'r') as client_json:
-            data = json.load(client_json)
-            for client in data:
-                client_obj = Client(client['Client name '])
-                client_obj.paid_training = client['paid training ']
-                client_obj.done_training = client['Complete training ']
-                self.client_list.append(client_obj)
+        if os.path.exists(self.__json_file_name):
+           with open(self.__json_file_name, 'r') as client_json:
+               data = json.load(client_json)
+               for client in data:
+                   client_obj = Client(client['Client name '])
+                   client_obj.paid_training = client['paid training ']
+                   client_obj.done_training = client['Complete training ']
+                   self.client_list.append(client_obj)
 
-    def load_to_json(self):
+
+
+    def save_to_json(self):
         with open(self.__json_file_name, 'w') as client_json:
             data = [client.Client_to_dict() for client in self.client_list]
             json.dump(data, client_json, ensure_ascii=False, indent=4)
@@ -55,8 +60,31 @@ class Clients():
     def Clients_info(self):
         cl_str = ""
         for client in self.client_list:
-            cl_str += f'Имя клиента:{client.name}\nОплачено занятий:{client.paid_training}\nПройдено занятий:{client.done_training}\n\n'
+            cl_str += (f'Имя клиента:{client.name}\n'
+                       f'Оплачено занятий:{client.paid_training}\n'
+                       f'Пройдено занятий:{client.done_training}\n'
+                       f'Депозит:{client.exp_money}\n'
+                       )
         return cl_str.strip()
+
+    def more_info(self):
+        cl_str = ""
+        for client in self.client_list:
+            cl_str += (f'Имя клиента:{client.name}\n'
+                       f'Оплачено занятий:{client.paid_training}\n'
+                       f'Пройдено занятий:{client.done_training}\n'
+                       f'Стоимость тренировки:{client.training_cost}\n'
+                       f'Депозит:{client.exp_money}\n'
+                       f'Заработано:{client.deposited_money}\n\n'
+                       )
+        return cl_str.strip()
+
+
+    def IsThereClients(self):
+        return len(self.client_list) != 0
+
+
+
 
 # if __name__ == "__main__":
 #     print("Запускай main_bot!!!")
